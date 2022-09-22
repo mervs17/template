5be15a6d9460302975f69318d402c788e356a589
mervs17 <merjs17@gmail.com>
2022-09-22 13:25:26 +0800

    Update ToggleDarkMode.js

--- src/components/ToggleDarkMode.js
+++ src/components/ToggleDarkMode.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
 
 export default function DarkMode() {
-  var toggleS = "Toggle__Switch";
+  const toggleS = "Toggle__Switch";
+
+  const [darkMode, setDarkMode] = useState(
+    () => JSON.parse(localStorage.getItem("site-dark-mode")) || false
+  );
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    const json = JSON.stringify(darkMode);
+    localStorage.setItem("site-dark-mode", json);
+    console.log("Dark Mode:", darkMode);
+  }, [darkMode]);
+
   return (
     <div className="Toggle">
       <p className="Toggle__Text Toggle--Light">Light</p>
       <div
-        id="toggleSwitch"
-        className={`${toggleS}--Light Toggle__Switch--Dark`}
+        onClick={() => setDarkMode((prevDarkMode) => !prevDarkMode)}
+        className={classNames(
+          toggleS,
+          !darkMode ? `${toggleS}--Light` : `${toggleS}--Dark`
+        )}
       ></div>
       <p className="Toggle__Text Toggle--Dark">Dark</p>
     </div>
